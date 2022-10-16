3e8ffef0ee19e1ceb08a276cef7c3425562a9cbe
Christian Rath-Ulrich <christian.rath-ulrich@digitaspixelpark.com>
2022-10-16 22:00:45 +0200

    add additional metahumans

--- src/Doctrine/Enum/MethaTyp.php
+++ src/Doctrine/Enum/MethaTyp.php
@@ -6,6 +6,25 @@ namespace App\Doctrine\Enum;
 enum MethaTyp: string
 {
     case ork = 'Ork';
+    case dryaden = 'Dryaden';
+    case enkanto = 'Enkanto';
+    case nächtliche = 'Nächtliche';
+    case wakyambis = 'Wakyambis';
+    case xapiri = 'Xapiri Thëpë';
+    case nartaki = 'Nartaki';
+    case hobgoblins = 'Hobgoblins';
+    case oger = 'Oger';
+    case oni = 'Oni';
+    case satyrn = 'Satyrn';
+    case fomoris = 'Fomoris';
+    case minotauren = 'Minotauren';
+    case riesen = 'Riesen';
+    case zyklopen = 'Zyklopen';
+    case gnome = 'Gnome';
+    case harumen = 'Harumen';
+    case korobokuru = 'Korobokuru';
+    case menehunen = 'Menehunen';
+    case querxe = 'Querxe';
     case elf = 'Elf';
     case troll = 'Troll';
     case zwerg = 'Zwerg';
